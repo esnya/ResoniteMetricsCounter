b5728cbb69fedf393cc3ab7b2761c3b82d967cfe
esnya <2088693+esnya@users.noreply.github.com>
2024-12-18 22:55:39 +0900

    feat: ✨️ Add new metric type: Connectors

--- ResoniteMetricsCounter/Patch/Metric_Profiler_Patch.cs
+++ ResoniteMetricsCounter/Patch/Metric_Profiler_Patch.cs
@@ -19,20 +19,29 @@ internal static class Metric_Profiler_Patch
     private static readonly StopwatchPool stopwatch = new();
 
     private static readonly Dictionary<string, MetricType> MetricTypeByMethod = new() {
-        { nameof(Component.InternalRunApplyChanges), MetricType.Changes },
-        { nameof(Component.InternalRunUpdate), MetricType.Updates},
         { "Slot_PhysicsWorldTransformChanged", MetricType.PhysicsMoved },
+        { nameof(Component.InternalRunUpdate), MetricType.Updates},
         { nameof(ProtoFluxNodeGroup.RunNodeChanges), MetricType.ProtoFluxContinuousChanges },
         { nameof(ProtoFluxNodeGroup.RunNodeUpdates), MetricType.ProtoFluxUpdates },
+        { nameof(Component.InternalRunApplyChanges), MetricType.Changes },
+        { nameof(IImplementable.InternalUpdateConnector), MetricType.Connectors }
     };
 
     static IEnumerable<MethodBase> TargetMethods()
     {
-        yield return AccessTools.Method(typeof(ComponentBase<Component>), nameof(ComponentBase<Component>.InternalRunApplyChanges));
-        yield return AccessTools.Method(typeof(ComponentBase<Component>), nameof(ComponentBase<Component>.InternalRunUpdate));
+        ResoniteMod.Debug("Patching method for PhysicsMoved");
         yield return AccessTools.Method(typeof(Collider), "Slot_PhysicsWorldTransformChanged");
+        ResoniteMod.Debug("Patching method for ProtoFluxContinuousChanges");
         yield return AccessTools.Method(typeof(ProtoFluxNodeGroup), nameof(ProtoFluxNodeGroup.RunNodeChanges));
+        ResoniteMod.Debug("Patching method for ProtoFluxUpdates");
         yield return AccessTools.Method(typeof(ProtoFluxNodeGroup), nameof(ProtoFluxNodeGroup.RunNodeUpdates));
+        ResoniteMod.Debug("Patching method for Updates");
+        yield return AccessTools.Method(typeof(ComponentBase<Component>), nameof(ComponentBase<Component>.InternalRunUpdate));
+        ResoniteMod.Debug("Patching method for Changes");
+        yield return AccessTools.Method(typeof(ComponentBase<Component>), nameof(ComponentBase<Component>.InternalRunApplyChanges));
+        ResoniteMod.Debug("Patching method for Connectors");
+        yield return AccessTools.Method(typeof(Slot), nameof(Slot.InternalUpdateConnector));
+        yield return AccessTools.Method(typeof(ImplementableComponent<IConnector>), nameof(ImplementableComponent<IConnector>.InternalUpdateConnector));
     }
 
     static void Prefix(out Stopwatch __state)
@@ -46,18 +55,32 @@ internal static class Metric_Profiler_Patch
         {
             var ticks = stopwatch.Release(__state);
 
+            if (__instance is Slot slot)
+            {
+                if (slot.IsLocalElement || slot.IsDisposed || slot.World.Focus != World.WorldFocus.Focused)
+                {
+                    return;
+                }
+
+                ResoniteMetricsCounterMod.Writer?.Add(
+                    slot.Name,
+                    slot,
+                    ticks,
+                    MetricTypeByMethod[__originalMethod.Name]
+                );
+            }
             if (__instance is Worker worker)
             {
                 if (worker.World.Focus != World.WorldFocus.Focused) return;
 
-                if (worker.Parent is not Slot slot || slot.IsLocalElement)
+                if (worker.Parent is not Slot workerSlot || workerSlot.IsLocalElement)
                 {
                     return;
                 }
 
                 ResoniteMetricsCounterMod.Writer?.Add(
                     worker.Name,
-                    slot,
+                    workerSlot,
                     ticks,
                     MetricTypeByMethod[__originalMethod.Name]
                 );
@@ -72,11 +95,11 @@ internal static class Metric_Profiler_Patch
                     return;
                 }
 
-                var slot = node.Slot;
+                var nodeSlot = node.Slot;
 
                 ResoniteMetricsCounterMod.Writer?.Add(
                     group.Name,
-                    slot,
+                    nodeSlot,
                     ticks,
                     MetricTypeByMethod[__originalMethod.Name]
                 );
