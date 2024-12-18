d5ac3085efb939af080dc872cb87cfeb972db602
esnya <2088693+esnya@users.noreply.github.com>
2024-12-18 18:08:33 +0900

    refactor: ⚡️ Separate StopwatchPool into a new class

---
+++ ResoniteMetricsCounter/Utils/StopwatchPool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ResoniteMetricsCounter.Utils;
+
+internal sealed class StopwatchPool
+{
+    readonly LinkedList<Stopwatch> pool = new();
+    internal Stopwatch GetAndStart()
+    {
+        if (pool.Count == 0)
+        {
+            return Stopwatch.StartNew();
+        }
+        var stopwatch = pool.Last.Value;
+        pool.RemoveLast();
+        stopwatch.Restart();
+        return stopwatch;
+    }
+
+    internal long Release(Stopwatch stopwatch)
+    {
+        stopwatch.Stop();
+        var result = stopwatch.ElapsedTicks;
+        pool.AddLast(stopwatch);
+        return result;
+    }
+
+}
